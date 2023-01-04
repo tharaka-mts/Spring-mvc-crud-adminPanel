2f3085177a3b3486a7f7fc7e3f8c37a852e7ef00
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-01-05 00:28:09 +0530

    pet Owner bean class

---
+++ src/main/java/com/adminPanel/beans/petOwner.java
@@ -0,0 +1,57 @@
+package com.adminPanel.beans;
+
+import java.io.Serializable;
+
+public class petOwner implements Serializable {
+    private int id;
+    private String firstname;
+    private String email;
+    private String lastname;
+
+    public petOwner(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    private String contact;
+
+}
