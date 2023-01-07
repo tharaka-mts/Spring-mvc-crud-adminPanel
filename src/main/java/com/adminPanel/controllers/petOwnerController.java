23aa83b8839984271fd1e65ceaee24bca779e2e3
Tharaka Sandaruwan <tharaka.maths@gmail.com>
2023-01-08 00:00:06 +0530

    petowner controller

--- src/main/java/com/adminPanel/controllers/petOwnerController.java
+++ src/main/java/com/adminPanel/controllers/petOwnerController.java
@@ -5,6 +5,7 @@ import com.adminPanel.dao.petOwnerDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,12 +17,40 @@ public class petOwnerController {
 
     @Autowired
     petOwnerDao petownerdao;
+    @RequestMapping("addOwner")
+    public String addOwner(){
+        return "addPetowner";
+    }
 
     @RequestMapping("petowners")
     public String view(Model model){
         List<petOwner> list = petownerdao.getOwner();
         model.addAttribute("list", list);
         return "viewPetowners";
+    }
+
+//    add petowner
+    @RequestMapping(value = "add-owner" , method = RequestMethod.POST)
+    public String add(@ModelAttribute("owner") petOwner owner){
+        if(petownerdao.addOwner(owner) == true){
+            return "redirect:addOwner?success=true";
+        } return "redirect:addOwner?success=false";
+
+    }
+
+//    edit pet owner
+
+    @RequestMapping(value = "edit-pet-owner/{id}")
+    public String editPage(@PathVariable int id, Model m){
+        petOwner owner = petownerdao.getOwnerById(id);
+        m.addAttribute("command", owner);
+        return "editPetowner";
+
+    }
+    @RequestMapping(value = "edit-save-owner")
+    public String editSubmit(@ModelAttribute("owner") petOwner owner ){
+        petownerdao.update(owner);
+        return "redirect:home";
     }
 
     @RequestMapping(value = "/delete-pet-owner/{id}", method = RequestMethod.GET)
